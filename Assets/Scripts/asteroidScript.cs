35a533a28d6ea017ad664e36e689ef71cbf6f2aa
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-11-01 11:11:16 +0000

    stuff

---
+++ Assets/Scripts/asteroidScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public GameObject asteroid;
+
+    System.Random rnd = new System.Random();
+
+    // Delete self when colliding with bullet or outer wall
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Bullet
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            Destroy(this.gameObject);
+        }
+
+        // Outer walls
+        if (collision.gameObject.name == "OuterWallUp")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.name == "OuterWallDown")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.name == "OuterWallLeft")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.name == "OuterWallRight")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start in a random position
+        transform.position = new Vector2(rnd.Next(-20, 20), rnd.Next(-9, 9));
+
+        // Move self
+        rb.AddForce(new Vector2(200, 200));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/spawnAsteroid.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class asteroidSummon : MonoBehaviour
+{
+    public GameObject[] asteroids;
+    float timer = 0;
+    bool timerReached = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (true)
+        {
+            spawnAsteroid();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*spawnAsteroid();
+        if (!timerReached)
+            timer += Time.deltaTime;
+
+        if (!timerReached && timer > 5)
+        {
+            spawnAsteroid();
+
+            // Set to false so that We don't run this again
+            timerReached = true;
+        }*/
+    }
+
+    void spawnAsteroid()
+    {
+        StartCoroutine(Spawn());   
+    }
+
+    IEnumerator Spawn()
+    {
+        // Summon clone of a random asteroid from the array
+        Instantiate(asteroids[Random.Range(0, asteroids.Length)]);
+
+        yield return new WaitForSeconds(5 /*Random.Range(0, 3)*/);
+    }
+}
