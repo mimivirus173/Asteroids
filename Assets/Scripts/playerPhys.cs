29083e6376697d7549ce210e50abb0cb17cccfb1
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-11-13 15:22:59 +0000

    Update playerPhys.cs

--- Assets/Scripts/playerPhys.cs
+++ Assets/Scripts/playerPhys.cs
@@ -14,6 +14,15 @@ public class Player : MonoBehaviour
     // Declare RigidBody
     private Rigidbody2D rb;
 
+    // Destroy upon contact with asteroids
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.StartsWith("Asteroid"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
