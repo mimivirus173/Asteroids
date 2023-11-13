44e41e2c61d1021ad52f273997417e14daaed55f
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2023-11-13 15:22:39 +0000

    test

--- Assets/Scripts/asteroidScript.cs
+++ Assets/Scripts/asteroidScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -9,23 +10,26 @@ public class asteroidScript : MonoBehaviour
     private Rigidbody2D rb;
     public GameObject asteroid;
     private GameObject player;
+    
+    // Variables for asteroid
     private int hp;
+    private int area;
+    private float speed;
 
     // Collisions
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        /* // Ignore inner walls
-        if (collision.gameObject.tag == "InnerWall")
+        // Spawn protector
+        if (collision.gameObject.name == "Spawn Protector")
         {
-            var innerWallCollider = collision.gameObject.GetComponent<BoxCollider2D>();
-            Physics2D.IgnoreCollision(innerWallCollider, GetComponent<PolygonCollider2D>());
-        } */
+            Destroy(this.gameObject);
+        }
 
         // Bullet
         if (collision.gameObject.name.StartsWith("Bullet"))
         {
             // Reduce HP
-            hp -= 2;
+            hp -= Random.Range(5, 10);
 
             // Delete object when HP = 0
             if (hp <= 0)
@@ -50,25 +54,27 @@ public class asteroidScript : MonoBehaviour
         // Start in a random position
         transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-9, 9));
 
-        // Edit shape
+        // Edit shape and declare area variable
         int sizeX = Random.Range(1, 5);
         int sizeY = Random.Range(1, 5);
         transform.localScale = new Vector3(sizeX, sizeY);
+        area = sizeX * sizeY;
 
-        // HP and mass set to size
-        hp = sizeX * sizeY;
-        rb.mass = sizeX * sizeY;
+        // Constants
+        hp = area * 5;
+        speed = 500 / Mathf.Sqrt(area);
+        rb.mass = area;
 
         // Movement and rotation
         player = GameObject.FindGameObjectWithTag("Player");                // Declare for player
-        transform.LookAt(player.transform.position, transform.forward);     // Face towards player
-        rb.AddForce(this.transform.forward * (5000 / (sizeX * sizeY)));     // Add force to "push" self towards player
+        transform.LookAt(player.transform.position, transform.forward);     // Face towards 
+        rb.AddForce(this.transform.forward * speed);                       // Add force to "push" self towards player
         transform.rotation = Quaternion.Euler(0, 0, 0);                     // Reset y-axis so the asteroid faces the correct way
         
         // Rotation
         rb.AddTorque
             (
-                Random.Range(-30, 30) * (3 + ((sizeX * sizeY) / 10))
+                Random.Range(-50, 50) * (3 + (area / 10))
             );
 
         // Find inner walls via tag
